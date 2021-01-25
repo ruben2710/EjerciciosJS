b362b578808917e089cbfc1babd03d8abef2f417
ruben2710 <77392528+ruben2710@users.noreply.github.com>
2021-01-25 13:12:25 +0100

    Ejercicio2cumpleanos

---
+++ Ejercicio2/index.js
@@ -0,0 +1,14 @@
+'use strict';
+
+// Guardar en un objeto informacion
+
+const celta {
+    day: 20;
+    month: 'abril';
+};
+console.log('Mi cumpleaños es el' + 'celta.day' + 'de' + 'celta.month');
+
+// Creamos array
+
+const meses = ['enero', 'febrero', 'marzo', 'abril', 'mayo', 'junio', 'julio', 'agosto', 'septiembre', 'octubre', 'noviembre', 'diciembre'];
+console.log(meses [4]);
