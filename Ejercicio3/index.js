cefa370169de90c07da7b34a2a6190db120513c9
ruben2710 <77392528+ruben2710@users.noreply.github.com>
2021-01-26 10:45:24 +0100

    Ejercicio3array

---
+++ Ejercicio3/index.js
@@ -0,0 +1,13 @@
+'use strict';
+
+const data = ['hola', 2, 5, 'adios'];
+
+// Determina cuál de los dos números que hay en el array es mayor. 
+console.log('5 es mayor que 2', data[2]>data[1]);
+
+// Determina el resultado de las cinco operaciones numéricas realizadas con los dos elementos numéricos. 
+console.log('Suma', data[1]+data[2]);
+console.log('Resta', data[1]-data[2]);
+console.log('Multiplicación', data[1]*data[2]);
+console.log('División', data[1]/data[2]);
+console.log('Cociente', data[1]%data[2]);
