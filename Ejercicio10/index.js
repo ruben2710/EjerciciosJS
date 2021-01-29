d532cc3f9062af04b6f898bc78996fd1a22c3fd4
ruben2710 <77392528+ruben2710@users.noreply.github.com>
2021-01-29 11:14:32 +0100

    Ejercicio10

---
+++ Ejercicio10/index.js
@@ -0,0 +1,8 @@
+
+const number = [253, 8575, 1, 20, 562, 1233, 25, 27, 258, 254, 7485, 2683];
+
+const pares = number.filter(array => array%2 === 0);
+const impares = number.filter(array => array%2 !== 0);
+
+console.log('pares', pares);
+console.log('impares', impares);
