ce9743834d6bd05383a5277d27b8f2a90191b782
ruben2710 <77392528+ruben2710@users.noreply.github.com>
2021-01-29 14:23:40 +0100

    Ejercicio12

---
+++ Ejercicio12/indes.js
@@ -0,0 +1,11 @@
+const words = ['Environmental', 'Systems', 'Research', 'Institute'];
+
+const maslarga = words.reduce((acumulador, cadena) => {
+  if (acc.length > current.length) {
+    return acumulador
+  } else {
+    return cadena
+  }
+});
+
+console.log(`La palabra más larga es ${maslarga}`);
