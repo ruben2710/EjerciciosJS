66832ea44dcb86921a3c302c9d06eb4596f769df
ruben2710 <77392528+ruben2710@users.noreply.github.com>
2021-01-30 16:01:11 +0100

    Ejercicio15

---
+++ Ejercicio15/index.js
@@ -0,0 +1,21 @@
+const resultados = [
+    {name: 'Christania Williams', country: 'Jamaica', time: 11.80},
+    {name: 'Elaine Thompson', country: 'Jamaica', time: 10.71},
+    {name: 'Tori Bowie', country: 'EEUU', time: 10.83},
+    {name: 'Marie Josée Ta Lou', country: 'Irlanda', time: 10.86},
+    {name: 'Dafne Schippers', country: 'Holanda', time: 10.90},
+    {name: 'English Gardner', country: 'EEUU', time: 10.94},
+    {name: 'Michelle-Lee Ahye', country: 'Trinidad', time: 10.92},
+    {name: 'JamaicaShelly-Ann Fraser-Pryce', country: 'Jamaica', time: 10.86}
+  ];
+
+  const menortiempo = resultados.reduce((acumulador, {time}) => {
+      if (acumulador < time) {
+          return acumulador
+      } else {
+          return time
+      }
+    }, {});
+
+  console.log(menortiempo);
+
